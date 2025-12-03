59de4e74721c68ce30253db269fd481ae622a85f
MateoCa10 <matcastilloma@uide.edu.ec>
2025-12-03 17:57:48 -0500

    Practica Semana6

---
+++ demo.js
@@ -0,0 +1,12 @@
+const ArrayList = require("../src/ArrayList");
+
+const arrayList = new ArrayList();
+
+arrayList.add(1);
+arrayList.add(2);
+arrayList.add(3);
+arrayList.add(4);
+arrayList.add(5);
+
+console.log(arrayList.toString());
+
